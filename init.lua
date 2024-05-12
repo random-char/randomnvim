0e8c2bc9630260dc84d00ca22e46c1401c9c895f
random-char <mailbox2bfq@gmail.com>
2024-05-12 12:15:57 +0200

    initial

---
+++ init.lua
@@ -0,0 +1,21 @@
+--config
+require("config")
+--config end
+
+--lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+local lazy = require("lazy")
+lazy.setup("plugins")
+--lazy end
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,22 @@
+return {
+	{ "none-ls-extras.nvim" },
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					--null_ls.builtins.completion.spell,
+					--require("none-ls.diagnostics.eslint"),
+					--php
+					null_ls.builtins.diagnostics.phpcs,
+					null_ls.builtins.diagnostics.phpstan,
+					null_ls.builtins.formatting.phpcsfixer,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+}
