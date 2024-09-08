aab0d4604f17cbda758a269aa738edd362ebaff2
random-char <mailbox2bfq@gmail.com>
2024-09-08 23:01:35 +0200

    minor readme update; adding telescope to code references view

--- README.md
+++ README.md
@@ -34,29 +34,29 @@ Keymaps
         <td colspan="2">Neo-tree</td>
     </tr>
     <tr>
-        <td>``&lt;leader&gt;e``</td>
+        <td>&lt;leader&gt;e</td>
         <td>Open Neo-tree</td>
     </tr>
     <tr>
         <td colspan="2">Oil.nvim</td>
     </tr>
     <tr>
-        <td>``&lt;leader&gt;o``</td>
+        <td>&lt;leader&gt;o</td>
         <td>open Oil</td>
     </tr>
     <tr>
-        <td>``&lt;CR>``</td>
+        <td>&lt;CR></td>
         <td>open dir/file</td>
     </tr>
     <tr>
-        <td>``-``&lt;/td>
+        <td>-&lt;/td>
         <td>go up a directory</td>
     </tr>
     <tr>
         <td colspan="2">Git</td>
     </tr>
     <tr>
-        <td>``&lt;leader&gt;gl``</td>
+        <td>&lt;leader&gt;gl</td>
         <td>LazyGit</td>
     </tr>
 </table>
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,65 +1,71 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                opts = {
-                    auto_install = true,
-                    ensure_installed = {
-                        -- golang
-                        "gopls",
-                        -- php 
-                        "phpactor",
-                        "psalm",
-                        -- js
-                        "eslint-lsp",
-                        "prettierd",
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				opts = {
+					auto_install = true,
+					ensure_installed = {
+						-- golang
+						"gopls",
+						-- php
+						"phpactor",
+						"psalm",
+						-- js
+						"eslint-lsp",
+						"prettierd",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local telescopt_builtin = require("telescope.builtin")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-            })
+			local lspconfig = require("lspconfig")
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.phpactor.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.phpactor.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.psalm.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.psalm.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.eslint.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
 
-            -- code
-            vim.keymap.set({ "n", "v" }, "<leader>cd", vim.lsp.buf.definition, { desc = "[D]efinitions" })
-            vim.keymap.set({ "n", "v" }, "<leader>ci", vim.lsp.buf.implementation, { desc = "[I]mplementations" })
-            vim.keymap.set({ "n", "v" }, "<leader>cr", vim.lsp.buf.references, { desc = "[R]eferences" })
+			-- code
+			vim.keymap.set({ "n", "v" }, "<leader>cd", vim.lsp.buf.definition, { desc = "[D]efinitions" })
+			vim.keymap.set({ "n", "v" }, "<leader>ci", vim.lsp.buf.implementation, { desc = "[I]mplementations" })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>cr",
+				telescopt_builtin.lsp_references,
+				{ desc = "[R]eferences", noremap = true, silent = true }
+			)
 
-            -- lsp
-            vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.rename, { desc = "[R]ename" })
-            vim.keymap.set({ "n", "v" }, "<leader>lh", vim.lsp.buf.hover, { desc = "[H]over" })
-        end,
-    },
+			-- lsp
+			vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.rename, { desc = "[R]ename" })
+			vim.keymap.set({ "n", "v" }, "<leader>lh", vim.lsp.buf.hover, { desc = "[H]over" })
+		end,
+	},
 }
